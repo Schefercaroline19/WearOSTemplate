9b7da556d181770eba9fb18b67ff4a5d59beec76
pontagrossa-ai <pontagrossa@ifpr.edu.br>
2026-06-16 17:06:43 -0300

    Primeira tela
    Tela para iniciar a corrida

--- app/src/main/java/com/ifpr/wearostemplate/presentation/MainActivity.kt
+++ app/src/main/java/com/ifpr/wearostemplate/presentation/MainActivity.kt
@@ -1,72 +1,117 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
- * most up to date changes to the libraries and their usages.
- */
-
 package com.ifpr.wearostemplate.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
-import androidx.wear.tooling.preview.devices.WearDevices
-import com.ifpr.wearostemplate.R
-import com.ifpr.wearostemplate.presentation.theme.WearOSTemplateTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
-
-        setTheme(android.R.style.Theme_DeviceDefault)
-
         setContent {
-            WearApp("Android")
+
+            var currentScreen by remember { mutableStateOf("home") }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
+                contentAlignment = Alignment.Center
+            ) {
+                // Relógio
+                TimeText()
+
+                // Alterna entre a tela inicial e a nova tela
+                if (currentScreen == "home") {
+                    RunningScreen(onPlayClick = { currentScreen = "detalhes" })
+                } else {
+                    DetailScreen()
+                }
+            }
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
-    WearOSTemplateTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
+fun RunningScreen(onPlayClick: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        androidx.compose.foundation.Image(
+            painter = androidx.compose.ui.res.painterResource(id = com.ifpr.wearostemplate.R.drawable.ic_run),
+            contentDescription = "Ícone de Corrida",
+            modifier = Modifier.size(70.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Iniciar corrida",
+            color = Color.White,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Normal
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+        Button(
+            onClick = onPlayClick,
+            modifier = Modifier.size(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Transparent,
+                contentColor = Color.White
+            ),
+            border = ButtonDefaults.outlinedButtonBorder(
+                borderColor = Color.White,
+                borderWidth = 2.dp
+            )
         ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
+            Text(
+                text = "▶",
+                fontSize = 26.sp,
+                color = Color.White
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
+fun DetailScreen() {
 
-@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "outra tela",
+            color = Color.White,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Normal
+        )
+    }
 }
